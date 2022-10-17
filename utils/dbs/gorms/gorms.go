f62deda4ca28985f67fef4160decd4fe5416392a
ann <davidgluttongk@outlook.com>
2022-10-17 17:38:34 +0800

    gorm client stater

---
+++ utils/dbs/gorms/gorms.go
@@ -0,0 +1,73 @@
+package gorms
+
+import (
+	"context"
+	"gin-boilerplate/utils/dbs"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
+)
+
+var GormDBs *gorm.DB
+
+// InitSimpleClient generates a simple gorm client
+func InitSimpleClient(cfg *gorm.Config) {
+	//dbCfg := configs.GetGlobalDbConfig()
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4", dbCfg.Usr, dbCfg.Pwd, dbCfg.Host, dbCfg.Port, dbCfg.DbName)
+	dsn := "root:pawword@tcp(127.0.0.1:3306)/test?charset=utf8mb4"
+	gormDB, err := gorm.Open(mysql.Open(dsn), cfg)
+
+	if err != nil {
+		//logs.Log.Error("gorm open error: ", err)
+	}
+
+	GormDBs = gormDB
+}
+
+func WithContext(ctx context.Context) *gorm.DB {
+	return GormDBs.WithContext(ctx)
+}
+
+// InitGormClient generates a gorm client.
+// If existingDb is true, it will use the existing db connection. Or it will create a new db connection.
+func InitGormClient(existingDb bool, customCfg bool) *gorm.DB {
+	if existingDb {
+		InitSimpleClient(getGormConfig(customCfg))
+	} else {
+		gormClient, err := gorm.Open(mysql.New(mysql.Config{
+			Conn: dbs.NativeClient,
+		}), getGormConfig(customCfg))
+		if err != nil {
+			//logs.Log.Panicln("Init gormClient failed!")
+		}
+
+		dbs.GormClient = gormClient
+	}
+
+	return nil
+}
+
+// GetGormConfig returns custom config if customCfg is true, or returns default config.
+func getGormConfig(customCfg bool) *gorm.Config {
+	if customCfg {
+		return &gorm.Config{
+			NowFunc: func() time.Time {
+				//return time.Now().Local()
+				return time.Now().UTC() // using UTC time
+			},
+			//Logger: logger.New(log.New(logs.Log.Out, "\r\n", log.LUTC), logger.Config{
+			//	LogLevel: logger.Error,
+			//}),
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+			SkipDefaultTransaction: false,
+		}
+	} else {
+		return &gorm.Config{}
+	}
+}
+
+//func InitClient(opt gorm.Option) *gorm.DB {
+//}
